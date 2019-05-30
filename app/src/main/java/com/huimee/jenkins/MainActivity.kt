cef8255b804bebb1106d0600c69ce3ce44670f47
yx <609549308@qq.com>
2019-05-30 17:01:19 +0800

    添加修改appID

--- app/src/main/java/com/huimee/jenkins/MainActivity.kt
+++ app/src/main/java/com/huimee/jenkins/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.huimee.jenkins
 
+import android.content.pm.PackageInfo
+import android.content.pm.PackageManager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
@@ -11,5 +13,13 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         but.text = BuildConfig.text
         but2.text = BuildConfig.num.toString()
+
+        val mPackageManager = this.packageManager
+        val info = mPackageManager.getPackageInfo(this.packageName, 0)
+
+        but3.text = info.packageName
+
     }
+
+
 }
