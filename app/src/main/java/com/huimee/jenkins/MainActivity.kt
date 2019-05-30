70cc3dec7c0a56730e3962b44b11e0935dbe309a
yx <609549308@qq.com>
2019-05-30 15:06:12 +0800

    修改参数

--- app/src/main/java/com/huimee/jenkins/MainActivity.kt
+++ app/src/main/java/com/huimee/jenkins/MainActivity.kt
@@ -9,6 +9,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        but.text = BuildConfig.text
+        but.text = BuildConfig.text.toString()
     }
 }
